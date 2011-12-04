71541c4abce6a44626f826bb6513ae8e5fdd71c6
kbl <m.pietraszek@gmail.com>
2011-12-05 00:33:40 +0100

    pending specs

--- lib/sdos/tweet_parser.rb
+++ lib/sdos/tweet_parser.rb
@@ -1,12 +1,13 @@
 module Sdos
   class TweetParser
- 
-    TWEET_PATTERN = /(\w*):.*?@(\w*)/
+
+    TWEET_PATTERN = /([a-z]*):.*?(@([a-z]*).*?)*/i
 
     class << self
       def parse_tweet(tweet)
         x = {}
         match_data = TWEET_PATTERN.match(tweet)
+        p match_data
         x[:author] = match_data[1]
         x[:mentioned] = match_data.to_a.slice(2..-1)
 
--- spec/lib/sdos/tweet_parser_spec.rb
+++ spec/lib/sdos/tweet_parser_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 
 module Sdos
   describe TweetParser do
-    it 'should properly parse author and mentioned user' do
-      input = 'alberta: hey @christie. what will we be reading at the book club meeting tonight?'
-      result = TweetParser.parse_tweet(input)
+    describe 'TweetParser.parse_tweet' do
+      it 'should properly parse author and mentioned user' do
+        pending
+        input = 'alberta: hey @christie. what will we be reading at the book club meeting tonight?'
+        result = TweetParser.parse_tweet(input)
 
-      result[:author].should == 'alberta'
-      result[:mentioned].should == %w(christie)
+        result[:author].should == 'alberta'
+        result[:mentioned].should == %w(christie)
+      end
+      it 'should parse tweet #2' do
+        pending
+        input = 'christie: "Every day, men and women, conversing, beholding and beholden..." /cc @alberta, @bob'
+        result = TweetParser.parse_tweet(input)
+
+        result[:author].should == 'christie'
+        result[:mentioned].should == %w(alberta bob)
+      end
     end
   end
 end
